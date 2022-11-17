10636cd551a8daadb41df594cd2f020405757257
rosemoss711 <rosemoss711@gmail.com>
2022-11-17 21:04:19 +0900

    add MUI2

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,8 +1,13 @@
-// import React, { useState, useEffect } from 'react';
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 // import axios from 'axios'
 import User from './components/User'
 import './App.css';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
 
 // const App = () => {
 //   // 요청받은 정보를 담아줄 변수 선언
@@ -61,21 +66,24 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
         { // map 사용시 key 설정 필수
           users.map(u => {
-            return (
-              <User
-                key={u.id}
-                id={u.id}
-                image={u.image}
-                name={u.name}
-                birthday={u.birthday}
-                gender={u.gender}
-                job={u.job}
-                />
-            );
-          })
+            return ( <User key={u.id} id={u.id} image={u.image} name={u.name} birthday={u.birthday} gender={u.gender} job={u.job} /> ); })
         }
+        </TableBody>
+        </Table>
       </div>
     );
   }
--- src/main/frontend/src/components/User.js
+++ src/main/frontend/src/components/User.js
@@ -1,37 +1,44 @@
 import React from 'react';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 class User extends React.Component {
     render() {
         return (
-            <div>
-                <UserProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <UserInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+            </TableRow>
+            
         )
     }
 }
 
-class UserProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
+// class UserProfile extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <img src={this.props.image} alt="profile"/>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
+//         )
+//     }
+// }
 
-class UserInfo extends React.Component {
-    render(){
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
+// class UserInfo extends React.Component {
+//     render(){
+//         return (
+//             <div>
+//                 <p>{this.props.birthday}</p>
+//                 <p>{this.props.gender}</p>
+//                 <p>{this.props.job}</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default User;
